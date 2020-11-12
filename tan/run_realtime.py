7bedd229c05b361e950ca70ba0b0118eedb18ef2
Brian Song <44029531+briansong524@users.noreply.github.com>
2020-11-12 10:38:44 -0800

    Update run_realtime.py

--- tan/run_realtime.py
+++ tan/run_realtime.py
@@ -26,3 +26,19 @@ def main():
 if __name__ == '__main__':
 	main()
 
+# from utils import *
+# conn_cred = {
+# 				    "dbservername":"localhost",
+# 				    "dbname":"main_schema",
+# 				    "dbuser":"minx",
+# 				    "dbpassword":"!xobILE!!!111!"
+# 				}
+# symbols = ['AAPL','TSLA','AMZN','GOOGL']
+# app = RealTimeTickApp()
+# app.connect("127.0.0.1", 7497, 1)
+# app.mysqlConfig(conn_cred)
+# for i in range(len(symbols)):
+# 	contract = basicContract(symbols[i])
+# 	app.start_reqRealTimeBars(i+1,contract,5,'TRADES',0,[])
+
+# app.reset()
