402c5f9ef04ae7ef7613f846cfcf01821317572c
Brian Song <44029531+briansong524@users.noreply.github.com>
2020-11-18 02:43:37 -0800

    update

--- tan/rsi_calculate_live.py
+++ tan/rsi_calculate_live.py
@@ -2,11 +2,6 @@
 # Calculate the RSI based on the aggregate market data from MySQL
 # Send message to Slack when the RSI reaches certain ranges of values
 #
-# TODO:
-# fix the time.sleep() logic
-# modify the rsi calculations
-# fix the last_datetime update logic
-# add a 'starting point' rsi/U/D that runs at startup
  
 import requests
 import time
@@ -31,17 +26,22 @@ def main(config):
 	# initialize
 	conn_cred = config['conn_cred']
 	slack_hook = config['slack_webhook']
-	symbols = ','.split(config['symbols'])
+	symbols = config['symbols'].split(',')
 	bar_range = config['bar_range']
 	info_dict = {}
 
-	for i in symbols:
+	print('initializing RSI calculations with last 250 data points')
+	for symbol in symbols:
 		# initialize a dict that keeps track of stuff 
-		info_dict[i] = {
-						'avgU':0,
-						'avgD':0,  
+		prevU, prevD, last_val = initilize_prevUD(conn_cred, bar_range, symbol)
+		info_dict[symbol] = {
+						'avgU':prevU,
+						'avgD':prevD,  
+						'last_val':last_val,
+						'last_epoch':0,
 						'last_message':0 # epoch of last message sent
 					   }  
+	print('Done initializing')
 
 	while True:
 		# constantly running until halted
@@ -51,74 +51,82 @@ def main(config):
 		df_cols = ['symbol','datetime','epoch','open','close',
 				   'high','low','volume']
 		df = pd.DataFrame(rows, columns = df_cols)
+		df['close'] = df['close'].astype(float)
+		df.set_index('symbol', inplace = True)
 
+		df_dict = df.to_dict('index')
 		for symbol in symbols:
 			try:
-				df_part = df[df['symbol'] == symbol].copy()
-				if df_part.shape[0] != 0:
+				
+				# values to measure rsi with
+				val = df_dict[symbol]['close'] - info_dict[symbol]['last_val']
+				info_dict[symbol]['last_val'] = df_dict[symbol]['close']
+				if info_dict[symbol]['last_epoch'] == df_dict[symbol]['epoch']:
+					rsi_, _, _ = calculate_rsi(val, prevU, prevD) 
+				else:
+					rsi_, prevU, prevD = calculate_rsi(val, prevU, prevD)
+				print(symbol + ' rsi: ' + str(round(rsi_,2)))
 					
-					# values to measure rsi with
-					vals = (df_part['close'] - df_part['open'])
-					vals = vals.astype(float).values
+				info_dict[symbol]['last_epoch'] = df_dict[symbol]['epoch']
 
-					if info_dict[symbol]['first_rsi']:
-						rsi_, prevU, prevD = calculate_rsi(vals) 
-						info_dict[symbol]['first_rsi'] = False
-						print('first rsi calculated for ' + 
-							  str(symbol) + ': ' + str(rsi_))
-					else:
-						if info_dict[symbol]['last_datetime'] == df['datetime'].iloc[0]:
-							rsi_, _, _ = calculate_rsi(vals, prevU, prevD) 
-
-						else:
-							print('rsi @ ' + str(info_dict[symbol]['last_datetime']) + ' = ' + str(rsi_))
-							rsi_, prevU, prevD = calculate_rsi(vals, prevU, prevD)
-						
-					info_dict[symbol]['last_datetime'] = df['datetime'].iloc[0]
-
-					# send slack message based on rsi
-					if (rsi_ <= 20) | (rsi_ >= 80):
-						text = symbol + ' hit RSI ' + str(round(rsi_,2)) + ' at ' + str(info_dict[symbol]['last_datetime'])
-						myobj = {"text":text}
+				# send slack message based on rsi
+				if (rsi_ <= 20) | (rsi_ >= 80):
+					text = symbol + ' hit RSI ' + str(round(rsi_,2)) + ' at ' + str(info_dict[symbol]['last_epoch'])
+					myobj = {"text":text}
+					if time.time() > info_dict[symbol]['last_message'] + 300: # five minutes after last message sent for specific symbol 
 						requests.post(slack_hook, json = myobj)
-						message_sent = True
+						info_dict[symbol]['last_message'] = time.time()
 
 			except Exception as e:
 				myobj = {"text":'something happened with ' + str(symbol) + ": " + str(e)}
 				requests.post(slack_hook, json = myobj)
 
 
-		# suppress slack messages for five minute if a message was sent
-		if message_sent:
-			time.sleep(300)
-			message_sent = False
-		else:
-			time.sleep(5)
+		time.sleep(5) # controlling the rate of the loop with 5 second delays
 
-def rsi_initialize(*args, **kwargs):
+def initilize_prevUD(conn_cred, bar_range, symbol):
 	# get last 100 close values
-	query = "WITH cte AS ( \
-			 SELECT close from {dbname}.bar_{bar_range}min \
-			 WHERE symbol = '{symbol}' \
-			 ORDER BY datetime_rounded DESC LIMIT 100 \
-			 ) \
-			 SELECT * FROM cte \
-			 ORDER BY datetime_rounded".format(
-			 			dbname = conn_cred['dbname'], 
-			   		    bar_range = bar_range,
-			   		    symbol = symbol)
-	run_query(query)
+	# only important values are prevU and prevD 
 
-def query_gen(*args, **kwargs):
-	query = ''
+	
+	query = "WITH cte AS ( \
+			 SELECT close, datetime_rounded from {dbname}.bar_{bar_range}min \
+			 WHERE symbol = '{symbol}' \
+			 ORDER BY datetime_rounded DESC LIMIT 250 \
+			 ) \
+			 SELECT close FROM cte \
+			 ORDER BY datetime_rounded".format(
+						dbname = conn_cred['dbname'], 
+						bar_range = bar_range,
+						symbol = symbol)
+	rows = run_query(conn_cred, query, selectBool = True) 
+	rows = np.array(list(map(lambda x: float(x[0]), rows)))
+	last_val = rows[-1]
+	rows = rows[1:] - rows[:-1]
+
+	# initial calculation
+
+	vals = rows[:9]
+	prevU = np.sum(vals * (vals > 0).astype(int)) / 9
+	prevD = -1 * np.sum(vals * (vals < 0).astype(int)) / 9
+
+	for i in range(9, len(rows)):
+		_, prevU, prevD = calculate_rsi(rows[i], prevU, prevD, 9)
+		
+	return prevU, prevD, last_val
+
+
+def query_gen(conn_cred, bar_range, symbols):
+	# get most recent value to calculate rsi with
+	query = 'WITH '
 	for symbol in symbols:
-		cte = "WITH {symbol} AS ( \
+		cte = "{symbol} AS ( \
 			   SELECT * FROM {dbname}.bar_{bar_range}min \
 			   WHERE symbol = '{symbol}' \
-			   ORDER BY datetime_rounded DESC LIMIT 9 \
+			   ORDER BY datetime_rounded DESC LIMIT 1 \
 			   )".format(dbname = conn_cred['dbname'], 
-			   			 bar_range = bar_range,
-			   			 symbol = symbol)
+						 bar_range = bar_range,
+						 symbol = symbol)
 		query += cte
 		if symbol != symbols[-1]:
 			query += ", "
@@ -130,18 +138,31 @@ def query_gen(*args, **kwargs):
 			query += " UNION "
 	return query
 
-def calculate_rsi(vals, prevU = 0, prevD = 0, n = 9):
-	alpha = 2 / (n+1) # exponential method
-	U = np.sum(vals * (vals > 0).astype(int)) / n
-	D = -1 * np.sum(vals * (vals < 0).astype(int)) / n
-	avgU = alpha * U + (1 - alpha) * prevU
-	avgD = alpha * D + (1 - alpha) * prevD
+# def calculate_rsi(vals, prevU = 0, prevD = 0, n = 9):
+# 	alpha = 2 / (n+1) # exponential method
+# 	U = np.sum(vals * (vals > 0).astype(int)) / n
+# 	D = -1 * np.sum(vals * (vals < 0).astype(int)) / n
+# 	avgU = alpha * U + (1 - alpha) * prevU
+# 	avgD = alpha * D + (1 - alpha) * prevD
 
-	prevU = avgU
-	prevD = avgD
-	rs = avgU / avgD
+# 	prevU = avgU
+# 	prevD = avgD
+# 	rs = avgU / avgD
+# 	rsi = 100.0 - 100.0 / (1 + rs)
+# 	return rsi, prevU, prevD
+
+
+def calculate_rsi(val, prevU = 0, prevD = 0, n = 9):
+	if val > 0:
+		avgU = (prevU*(n-1) + val) / n
+		avgD = prevD*((n-1)/n)
+	else:
+		avgU = prevU*((n-1)/n)
+		avgD = (prevD*(n-1) - val) / n
+		
+	rs = avgU / (avgD + 1e-5)
 	rsi = 100.0 - 100.0 / (1 + rs)
-	return rsi, prevU, prevD
+	return rsi, avgU, avgD
 
 
 if __name__ == '__main__':
