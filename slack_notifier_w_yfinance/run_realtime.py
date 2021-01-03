3eed496d7d2168ecc83c12048316892c2e6000f5
Brian Song <44029531+briansong524@users.noreply.github.com>
2021-01-03 11:11:10 -0800

    Update run_realtime.py

--- slack_notifier_w_yfinance/run_realtime.py
+++ slack_notifier_w_yfinance/run_realtime.py
@@ -20,52 +20,61 @@ parser.add_argument(
 	'--config', type=str, default='/home/minx/Documents/config.conf',
 	help = 'Config file for all configuration info')
 
+
+def mod_time():
+	# return the hour modulo of epoch time
+	# the returned value represents the seconds passed that day
+	current_time = time.time()
+	return np.floor(current_time % 86400)
+
 def main(config):
 	last_dt = 0
 	try:
 		conn_cred = config['conn_cred']
 		symbols = config['symbols'].split(',')
-		while True:
-			
-			# define to run only during regular trading hours
-			current_time = time.time()
-			open_seconds = 14.5*60*60. # 2:30pm GMT
-			close_seconds = 21*60*60. # 9pm GMT
-			
-			if  >= open_seconds 
-			curr_time = time.time()
-			mod_time = np.floor(curr_time % 60)
-			if (mod_time >= 30) & (mod_time <= 32):
-				data = yf.download(tickers = config['symbols'].replace(',',' '), period = '1d', 
-								   interval = '1m', group_by = 'ticker')
+	
+		# define to run only during regular trading hours
+		open_seconds = 14.5*60*60. # 2:30pm GMT
+		close_seconds = 21*60*60. # 9pm GMT	
+		mark_epoch = open_seconds + 30 ## 30 seconds after opening	
 
-				# dropping incomplete data
-				data = data.dropna() 
+		while (mod_time() >= open_seconds) & (mod_time() <= close_seconds): 
+			while mark_epoch > mod_time():
+				time.sleep(5)
+
+			# update mark_epoch when it breaks the while loop
+			mark_epoch += 60 # add a minute
+
+			data = yf.download(tickers = config['symbols'].replace(',',' '), period = '1d', 
+							   interval = '1m', group_by = 'ticker')
+
+			# dropping incomplete data
+			data = data.dropna() 
+			for symbol in symbols:
+				data = data[data[(symbol,'Volume')] != 0] # this maybe overkill
+			latest_dt = data.index[-1] # index contains datetime for multi symbols
+			if latest_dt != last_dt:
+				last_dt = latest_dt
 				for symbol in symbols:
-					data = data[data[(symbol,'Volume')] != 0] # this maybe overkill
-				latest_dt = data.index[-1] # index contains datetime for multi symbols
-				if latest_dt != last_dt:
-					last_dt = latest_dt
-					for symbol in symbols:
-						latest_data = data[symbol].iloc[-1,:].copy()
-						# print(latest_data)
-						bardata = (convert_dt_to_epoch(last_dt),
-								   latest_data.Open, 
-								   latest_data.High, 
-								   latest_data.Low, 
-								   latest_data.Close, 
-								   latest_data.Volume
-								   )
-						# print(bardata)
-						csvOutputs = ','.join(map(lambda x: "'" + str(x) + "'",bardata))
-						query = 'INSERT INTO {dbname}.bar_data_yf (symbol, epoch, open, high, \
-								 low, close, volume) \
-								 VALUES ({symbol},{csv})'.format(dbname = conn_cred['dbname'],
-								 					  symbol = "'" + symbol + "'",
-													  csv = csvOutputs)
-						run_query(conn_cred, query)
-					time.sleep(3) # stops checking for this minute
-			
+					latest_data = data[symbol].iloc[-1,:].copy()
+					# print(latest_data)
+					bardata = (convert_dt_to_epoch(last_dt),
+							   latest_data.Open, 
+							   latest_data.High, 
+							   latest_data.Low, 
+							   latest_data.Close, 
+							   latest_data.Volume
+							   )
+					# print(bardata)
+					csvOutputs = ','.join(map(lambda x: "'" + str(x) + "'",bardata))
+					query = 'INSERT INTO {dbname}.bar_data_yf (symbol, epoch, open, high, \
+							 low, close, volume) \
+							 VALUES ({symbol},{csv})'.format(dbname = conn_cred['dbname'],
+							 					  symbol = "'" + symbol + "'",
+												  csv = csvOutputs)
+					run_query(conn_cred, query)
+			else:
+				print('last_dt == latest_dt, so didnt update this minute block')
 	except Exception as e:
 		print('error in run_realtime: ' + str(e))
 
