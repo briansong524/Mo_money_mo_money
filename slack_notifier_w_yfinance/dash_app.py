9fec13ff49d41b3fad84f7909c9454de92326872
Brian Song <44029531+briansong524@users.noreply.github.com>
2021-05-09 17:57:38 -0700

    Create dash_app.py

---
+++ slack_notifier_w_yfinance/dash_app.py
@@ -0,0 +1,33 @@
+''' Dash Frontend For Control 
+
+Have a frontend to configure config.json without the need to login to the 
+aws/gcp instance
+
+'''
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+app = dash.Dash()
+colors = {
+    'background': '#000000',
+    'text': '#7FDBFF'
+}
+app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+    html.H1(
+        children='RSI Notifier Configuration',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    ),
+    html.Label("Add Symbols",
+        style={
+            'color':colors['text']        
+        }),
+    dcc.Input(value='', type='text')
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
