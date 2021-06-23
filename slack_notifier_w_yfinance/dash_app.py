33e5088be48a00907733a2e53d3307a705d32f43
Brian Song <44029531+briansong524@users.noreply.github.com>
2021-06-23 15:27:27 -0700

    update dash app

--- slack_notifier_w_yfinance/dash_app.py
+++ slack_notifier_w_yfinance/dash_app.py
@@ -10,16 +10,19 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 import plotly.express as px
+import plotly.graph_objects as go
 
 import yfinance as yf
-from pytz import timezone
+import pytz
+import pandas as pd
 import numpy as np
 from datetime import datetime, timedelta
+import time
+import requests
 
 from sklearn.linear_model import LinearRegression
 
-
-from utils import midpoint_imputation, simple_lr, calculate_rsi
+from utils import midpoint_imputation, simple_lr, calculate_rsi, mult_rsi
 
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -28,7 +31,20 @@ colors = {
     'background': '#000000',
     'text': '#7FDBFF'
 }
+timezone_names = ['New York (GMT-5/GMT-4 DST)','Chicago (GMT-6/GMT-5 DST)',
+                  'Denver (GMT-7/GMT-6 DST)','Los Angeles (GMT-8/GMT-7 DST)',
+                  'Anchorage (GMT-9/GMT-8 DST)','Honolulu (GMT-10)']
+# pytz_names = ['America/New_York','America/Chicago',
+#               'America/Denver','America/Los_Angeles',
+#               'America/Anchorage','America/Honolulu']
+# timezones = zip(timezone_names, pytz_names)
 
+is_dst = time.localtime().tm_isdst == 1 
+if is_dst:
+    adjust = list(map(str,reversed(range(-9,-4)))) + ['-10']
+else:
+    adjust = list(map(str,reversed(range(-8,-3)))) + ['-10']
+timezones = zip(timezone_names, adjust)
 
 ## Page Layout
 app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
@@ -40,28 +56,51 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
         }
     ),
     html.Div(children=[
-        html.Label("Symbol: ",
-            style={
-                'color':colors['text']        
-            }),
-        dcc.Input(id='symbol', value='SPY', type='text')
-    ]),
-    html.Div(children=[
-        html.Label("Graph Style ('Line' or 'Bar'): ",
-            style={
-                'color':colors['text']        
-            }),
-        dcc.Input(id='graph-style', value='Line', type='text')
-    ]),
-    html.Div(children=[
-        html.Label("New Bar Size (minutes) [1,2,5,15,30]: ",
-            style={
-                'color':colors['text']        
-            }),
-        dcc.Input(id='bar-size', value='15', type='text')
-    ]),
-
-    html.Button(id='submit-button-state', n_clicks=0, children='Submit'),
+        html.Div(children=[
+            html.Label("Symbol: ",
+                style={
+                       'color':colors['text']        
+                       }
+            ),
+            dcc.Input(id='symbol', value='SPY', type='text')
+        ]),
+        html.Div(children=[
+            html.Label("Graph Style ('Line' or 'Bar'): ",
+                style={
+                       'color':colors['text']        
+                       }
+            ),
+            dcc.Input(id='graph-style', value='Bar', type='text')
+        ]),
+        html.Div(children=[
+            html.Label("New Bar Size (minutes) [1,2,5,15,30]: ",
+                style={
+                      'color':colors['text']        
+                      }
+            ),
+            dcc.Input(id='bar-size', value='5', type='text')
+        ]),
+        html.Div(children=[
+            html.Label("Timezone: ",
+                style={
+                      'color':colors['text']        
+                      }
+            ),
+            dcc.Dropdown(id='timezone', 
+                         options = [{'label':i,'value':j} for i,j in timezones],
+                         value='-4' if is_dst else '-5'
+                        )
+        ]),
+        html.Div(
+            html.Button(id='submit-button-state', n_clicks=0, children='Submit',
+                style={
+                       'margin-top':'5px',
+                       'margin-bottom':'3px'
+                       }
+            )
+        )],
+        style={'margin-left':'2px'}
+    ),
 
     # html.Div(id='output-state', style={
     #             'color':colors['text']        
@@ -74,32 +113,60 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
     Input('submit-button-state','n_clicks'), #n_clicks needed to update
     State('symbol','value'),
     State('graph-style','value'),
-    State('bar-size','value')
+    State('bar-size','value'),
+    State('timezone','value')
     )
-def update_config(button, symbol, graph_style, bar_size):
-    ticker = str(symbol)
-    period = '1mo'
-    interval = str(bar_size) + 'm'
-    data = yf.download(tickers = ticker, period = period, interval = interval, 
-                        group_by = 'ticker', prepost = True).reset_index()
-    print(ticker,graph_style,period,interval,' seems to work')
-    data['Upper'] = data[['Open','Close']].max(axis = 1)
-    data['Lower'] = data[['Open','Close']].min(axis = 1)
-    data['Midpoint'] = data[['Open','Close']].mean(axis = 1)
-    vals = data['Datetime']
-    v1 = vals.iloc[1:]
-    v2 = vals.iloc[:-1]
-    vals = list(map(lambda i: (v1.iloc[i]-v2.iloc[i]).total_seconds() / 60. / 60., range(len(v1))))
-    vals = [0] + vals
-    data['New Day'] = list(map(lambda x: x > 0.25, vals))
-    data['pos'] = data['Datetime'].map(lambda x: (x - data['Datetime'].iloc[0]).total_seconds() / 60 / 60) # for linear regression
-    x = data['pos']
-    lr1, m, b = simple_lr(np.array(x).reshape(-1, 1), data['Midpoint'])
+def market_graph(button, symbol, graph_style, bar_size, timezone):
+    timezone = int(timezone)
+    adjust_hours = timedelta(hours = timezone)
+    # send data info
+    data_url = 'http://localhost' + ':8001/'
+    params = {'symbol':symbol,
+              'bar_size':bar_size
+             }
+    response = requests.get(url = data_url, params = params).json()
+
+    assert str(response) == '200', 'Data did not load properly'
+
+    params = {'cols':'Datetime,Open,High,Low,Close,pos,Midpoint'}
+    data = requests.get(url = data_url + 'data', params = params).json()['data']
+    data = pd.DataFrame(data)
+    print(data.head())
+    print(adjust_hours)
+    data['Datetime'] = data['Datetime'].map(lambda x: \
+                            datetime.fromtimestamp(x/1000., pytz.utc) + adjust_hours) #\
+                            # .replace(tzinfo = pytz.timezone('America/New_York'))) #\
+                            # .astimezone(pytz.timezone(timezone)))
+    print(data.head())
+    lr3 = three_pass_lr(data)
+
+
+    # if graph_style == 'Line':
+    #     fig = px.scatter(x = data['Datetime'], y = data['rsi'])
+    if graph_style == 'Bar':
+        fig = go.Figure(data=[go.Candlestick(x=data['Datetime'],
+                open=data['Open'],
+                high=data['High'],
+                low=data['Low'],
+                close=data['Close'])])
+        y_trace = lr3.predict(data['pos'].values.reshape(-1,1))
+        fig.add_trace(go.Scatter(x=data['Datetime'], y=y_trace,
+                            mode='lines',
+                            name='regression line',
+                            marker_color='rgba(0,153,255,0.5)'))
+    return fig
+
+
+def three_pass_lr(df):
+    # run lr three times to build a lr model with volatile points reduced
+
+    x = df['pos']
+    lr1, m, b = simple_lr(np.array(x).reshape(-1, 1), df['Midpoint'])
     pred = lr1.predict(np.array(x).reshape(-1,1)) 
-    data['prop_dist_1'] = (pred - data['Midpoint']).abs() / pred
-    cutoff = np.percentile(data['prop_dist_1'], 65)
-    cutoff2 = np.percentile(data['prop_dist_1'], 50)
-    df = data[data['prop_dist_1'] < cutoff].copy()
+    df['prop_dist_1'] = (pred - df['Midpoint']).abs() / pred
+    cutoff = np.percentile(df['prop_dist_1'], 65)
+    cutoff2 = np.percentile(df['prop_dist_1'], 50)
+    df = df[df['prop_dist_1'] < cutoff].copy()
     x = np.array(df['pos']).reshape(-1,1)
     lr2, m, b = simple_lr(x, df['Midpoint'])
     pred = lr2.predict(np.array(x).reshape(-1,1))
@@ -108,49 +175,7 @@ def update_config(button, symbol, graph_style, bar_size):
     df2 = df[df['prop_dist_2'] < cutoff2].copy()
     x = np.array(df2['pos']).reshape(-1,1)
     lr3, m3, b3 = simple_lr(x, df2['Midpoint'])
-    pred = lr3.predict(data['pos'].values.reshape(-1,1))
-    data['epsilon'] = data['Midpoint'] - pred
-    # fig = px.scatter(x=data['pos'].values, y=pred)
-    # fig.update_layout(margin={'l': 40, 'b': 40, 't': 10, 'r': 0}, hovermode='closest')
-
-# ##plot bar chart
-# import plotly.graph_objects as go
-
-# fig = go.Figure(data=[go.Candlestick(x=data['Datetime'],
-#                 open=data['Open'],
-#                 high=data['High'],
-#                 low=data['Low'],
-#                 close=data['Close'])])
-# y_trace = lr3.predict(data['pos'].values.reshape(-1,1))
-# fig.add_trace(go.Scatter(x=data['Datetime'], y=y_trace,
-#                     mode='lines',
-#                     name='lines'))
-# fig.show()
-
-    rsi_list = []
-    n = 14
-    rows = data['Close'].values
-    rows = midpoint_imputation(rows)
-    # print(rows[-5:])
-    rows = rows[1:] - rows[:-1] # make prices to deltas
-
-    # initial calculation
-    vals = rows[:n]
-    prevU = np.sum(vals * (vals > 0).astype(int)) / n
-    prevD = -1 * np.sum(vals * (vals < 0).astype(int)) / n
-
-    for i in range(n, len(rows)):
-        rsi, prevU, prevD = calculate_rsi(rows[i], prevU, prevD, n)
-        rsi_list.append(rsi)
-
-    # modify shape to match indices with original data
-    rsi_list_modified = [np.nan] * (data.shape[0] - len(rsi_list)) + rsi_list
-    rsi = np.array(rsi_list_modified)
-    fig = px.scatter(x = range(len(rsi)), y = rsi)
-    return fig
-
-
-
+    return lr3
 
 if __name__ == '__main__':
     app.run_server(debug=True)
---
+++ slack_notifier_w_yfinance/dash_app_backend_flask.py
@@ -0,0 +1,146 @@
+import os
+import sys
+import traceback
+import time
+if sys.version_info[0] == 2:
+    import thread
+else:
+    import _thread as thread
+
+import json
+import argparse
+from datetime import datetime
+
+import pandas as pd
+import numpy as np
+from flask import Flask, render_template, request
+from flask import flash, redirect, session, abort
+import yfinance as yf
+
+import logging
+from logging.handlers import RotatingFileHandler
+
+from utils import midpoint_imputation, simple_lr, calculate_rsi, mult_rsi
+
+app = Flask(__name__)
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    '--port', type=int, default= 8001,
+    help = 'What port to host API server on.')
+
+################### logger config ###################
+global handler, logger, console_handler
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = RotatingFileHandler('app_client.log', maxBytes=10485760, backupCount=5)
+handler.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.addHandler(console_handler)
+#####################################################
+
+@app.route('/heart-beat', methods=['GET'])
+def run_heart_beat_check():  
+    status_object = {'status':'alive'}
+    response = app.response_class(
+        response=json.dumps(status_object),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+@app.route('/', methods = ['GET','POST'])
+def data_load():
+    global data_global
+    # load dataset via POST request
+    try:
+        # print(request.args)
+        # print(request.form)
+        symbol = request.args.get('symbol')
+        bar_size = request.args.get('bar_size')
+        try:
+            print(data_global['Datetime'][-1])
+            diff = datetime.now().timestamp() - data_global['Datetime'][-1]/1000
+        except Exception as e:
+            print('Exception in checking time: ' + str(e))
+            diff = 1e7
+        if diff < int(bar_size)*60.:
+            return '200' 
+        else:
+
+            ticker = str(symbol)
+            interval = str(bar_size) + 'm'
+            period = period_by_barsize(int(bar_size))
+            data = yf.download(tickers = ticker, period = period, interval = interval, 
+                                group_by = 'ticker', prepost = True).reset_index()
+            data = data_expand(data)
+
+            # rsi
+            n = 14
+            rows = data['Close'].values
+            rows = midpoint_imputation(rows)
+            rsi_list = mult_rsi(rows, n)
+
+            # modify shape to match indices with original data
+            rsi_list_modified = [-1] * (data.shape[0] - len(rsi_list)) + rsi_list
+            data['rsi'] = np.array(rsi_list_modified)
+            print(data.head(10))
+            data_as_json = data.to_json(orient='split')
+            data_as_json = eval(data_as_json)
+            vals_as_cols = zip(*data_as_json['data'])
+            data_global = {}
+            for i in data_as_json['columns']:
+                data_global[i] = next(vals_as_cols)
+            return '200'
+    except Exception as err:
+        type_, value_, traceback_ = sys.exc_info()
+        tb = traceback.format_exception(type_, value_, traceback_)
+        err_string = ' '.join(tb) + '\nException in getting data: ' + repr(err)
+        print(err_string)
+        return '400'
+
+@app.route('/data', methods = ['GET'])
+def get_data():
+    cols = str(request.args.get('cols'))
+
+    out_ = {}
+    for i in cols.split(','):
+        out_[i] = data_global[i]
+
+    return {'data':out_}
+
+def period_by_barsize(bar_size):
+    if bar_size <= 5:
+        return '1d'
+    elif bar_size in [15,30]:
+        return '5d'
+    else:
+        return '1mo'
+
+def data_expand(df):
+    df['Upper'] = df[['Open','Close']].max(axis = 1)
+    df['Lower'] = df[['Open','Close']].min(axis = 1)
+    df['Midpoint'] = df[['Open','Close']].mean(axis = 1)
+    vals = df['Datetime']
+    v1 = vals.iloc[1:]
+    v2 = vals.iloc[:-1]
+    vals = list(map(lambda i: (v1.iloc[i]-v2.iloc[i]).total_seconds() / 60. / 60., range(len(v1))))
+    vals = [0] + vals
+    df['New Day'] = list(map(lambda x: x > 0.25, vals)); df['New Day'] = df['New Day'].astype(int)
+    df['pos'] = df['Datetime'].map(lambda x: (x - df['Datetime'].iloc[0]).total_seconds() / 60 / 60) # for linear regression
+    return df
+
+
+
+
+if __name__ == '__main__':
+    global data
+    FLAGS, unparsed = parser.parse_known_args()
+    app.run(debug=True, port=FLAGS.port, host='0.0.0.0')
